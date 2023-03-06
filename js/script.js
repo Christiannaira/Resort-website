3d410d2f8bf0f08fc4fb57402d4d18d85f34c6ca
christiannairabuild <125229074+Christiannaira@users.noreply.github.com>
2023-03-06 13:04:54 +0630

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.hero-about-content', { delay: 200, origin: 'top' });
